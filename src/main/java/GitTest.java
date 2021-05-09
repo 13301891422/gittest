d2cf039c402ff108cc8aa8a87fce8966b1a770d4
13301891422 <xuweiwei5788@163.com>
2021-05-09 14:11:08 +0800

    Master 第二次提交

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("gitTest1");
         System.out.println("gitTest2 Back up Commit");
+        System.out.println("gitTest3 Master Commit");
     }
 }
